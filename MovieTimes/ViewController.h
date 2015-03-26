56a3ea4cb292cd498aa080665e39df5ec154321b
Justin <jmguarino@sonic.net>
2015-03-25 21:53:12 -0700

    Initial Commit

---
+++ MovieTimes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MovieTimes
+//
+//  Created by Justin Guarino on 3/25/15.
+//  Copyright (c) 2015 JustinGuarino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
